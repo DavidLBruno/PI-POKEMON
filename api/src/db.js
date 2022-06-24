7e26027cd162ead964d7423b94a87d10c92395bf
BRUNODAVID <dzou@company.com>
2022-06-24 15:06:06 -0300

    HEROKU 3

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,7 @@ let  sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
         { logging: false, native: false }
       );
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
