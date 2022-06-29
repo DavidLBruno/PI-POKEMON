588fb93657b830ca935c81e13899f266ba39b374
BRUNODAVID <dzou@company.com>
2022-06-28 23:42:20 -0300

    1234

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/index';
+import axios from 'axios';
 
 ReactDOM.render(
   <Provider store = { store }>
